d298748f2d6913eb1e5257425d5afccdb6c6f7e4
Boffka92 <boffka92@ya.ru>
2024-04-05 23:14:24 +0300

    Added SecurityConfiguration and protection on methods

--- src/main/java/ru/netology/SecurityConfiguration.java
+++ src/main/java/ru/netology/SecurityConfiguration.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity (securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
@@ -24,22 +26,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("Admin")
                 .password(encoder().encode("000000"))
-                .authorities("admin")
+                .roles("READ", "WRITE")
                 .and()
-                .withUser("User")
-                .password(encoder().encode("111111"))
-                .authorities("user");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
+                .withUser("User_read")
+                .password(encoder().encode("000000"))
+                .roles("READ")
                 .and()
-                .authorizeHttpRequests().antMatchers(HttpMethod.GET, "/persons/by-name-surname").hasAnyAuthority("admin")
-                .and()
-                .authorizeHttpRequests().antMatchers(HttpMethod.GET, "/persons/by-city").permitAll()
-                .and()
-                .authorizeHttpRequests().anyRequest().authenticated();
+                .withUser("User_delete")
+                .password(encoder().encode("000000"))
+                .roles("DELETE");
     }
 
 }
--- src/main/java/ru/netology/controller/PersonController.java
+++ src/main/java/ru/netology/controller/PersonController.java
@@ -1,5 +1,8 @@
 package ru.netology.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.netology.entity.Person;
 import ru.netology.service.PersonService;
 
+import javax.annotation.security.RolesAllowed;
 import javax.crypto.spec.OAEPParameterSpec;
 import java.util.List;
 import java.util.Optional;
@@ -21,22 +25,32 @@ public class PersonController {
         this.service = service;
     }
 
+    @Secured({"ROLE_READ"})
     @GetMapping("/persons/by-city")
     @ResponseBody
     public List<Person> getProduct(@RequestParam("city") String city) {
         return service.getPersonsByCity(city);
     }
 
+    @RolesAllowed({"ROLE_WRITE"})
     @GetMapping("/persons/by-age")
     @ResponseBody
     public List<Person> getProduct(@RequestParam("age") int age) {
         return service.getPersonsByAgeLessThan(age);
     }
 
+    @PostAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
     @GetMapping("/persons/by-name-surname")
     @ResponseBody
     public Optional<Person> getProduct(@RequestParam("name") String name, @RequestParam("surname") String surname) {
         return service.getPersonByNameAndSurname(name, surname);
     }
+
+    @PreAuthorize("#username == authentication.principal.username")
+    @GetMapping("/special")
+    public String specialUser(String username) {
+        return "Hi, special, " + username;
+    }
+
 
 }
